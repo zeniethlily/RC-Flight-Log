b6ddd2837b2c94f0988f6afb22c91cbe126d4be0
Darrell Yeo <zeniethlily@gmail.com>
2020-07-22 00:30:25 +0800

    more bootsrap

--- public/js/script.js
+++ public/js/script.js
@@ -3,3 +3,14 @@ $(function(){
         $(".alert").fadeOut("slow");
       }, 3000);
 });
+
+$(document).ready(function() {
+
+  const $valueSpan = $('.valueSpan');
+  const $value = $('#duration');
+  $valueSpan.html($value.val());
+  $value.on('input change', () => {
+
+    $valueSpan.html($value.val());
+  });
+});
